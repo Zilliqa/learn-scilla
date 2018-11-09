51849510b46ee0a153d8e775e69d881b10ec84ef
noelyoo <innovation.noel@gmail.com>
2018-11-10 00:49:31 +0900

    Hide answer code when hint is enabled

--- src/components/code-diff/index.tsx
+++ src/components/code-diff/index.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { MonacoDiffEditor } from 'react-monaco-editor';
+import './index.css';
 
 interface IProps {
   original: string;
   code: string;
+  showAnswer: boolean;
 }
 
 const CodeDiff: React.SFC<IProps> = (props) => (
-  <MonacoDiffEditor
-    width="600"
-    height="250"
-    language="javascript"
-    original={props.original}
-    value={props.code}
-    options={{
-      readOnly: true,
-      renderSideBySide: false,
-      cursorStyle: 'line'
-    }}
-  />
+  <div className={`diff ${props.showAnswer ? 'hide-answer' : ''}`}>
+    <MonacoDiffEditor
+      width="600"
+      height="250"
+      language="javascript"
+      original={props.original}
+      value={props.code}
+      options={{
+        readOnly: true,
+        renderSideBySide: false
+      }}
+    />
+  </div>
 );
 
 export default CodeDiff;
