11785c9c38d07658474f29019c0d1b7fe2a9103b
noelyoo <innovation.noel@gmail.com>
2018-11-10 03:07:16 +0900

    Handle lesson data as props

--- src/components/code-lesson/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
-
-interface IProps {
-  lesson: string;
-}
-
-const CodeLesson: React.SFC<IProps> = (props) => <ReactMarkdown source={props.lesson} />;
-export default CodeLesson;
---
+++ src/components/code-repl/index.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Col, Row } from 'reactstrap';
+
+import CodeInstruction from './code-intruction';
+import CodeEditor from './code-editor';
+import CodeDiff from './code-diff';
+
+interface IProps {
+  t: (key: string) => string;
+  initialCode: string;
+  answerCode: string;
+  instruction: string;
+}
+
+interface IState {
+  code: string;
+  codeForDiff: string;
+  showAnswer: boolean;
+}
+
+export default class CodeREPL extends React.Component<IProps, IState> {
+  public readonly state = {
+    code: this.props.initialCode,
+    codeForDiff: this.props.answerCode,
+    showAnswer: false
+  };
+
+  public render(): React.ReactNode {
+    const { t, instruction, answerCode } = this.props;
+    const { code, codeForDiff, showAnswer } = this.state;
+    return (
+      <Row>
+        <Col xs={12} sm={12} md={5} lg={5}>
+          <CodeInstruction instruction={instruction} />
+        </Col>
+        <Col xs={12} sm={12} md={7} lg={7}>
+          <CodeEditor
+            code={code}
+            submitCode={this.submitCode}
+            showAnswer={showAnswer}
+            checkAnswer={this.checkAnswer}
+            toggleShowAnswer={this.toggleShowAnswer}
+            t={t}
+          >
+            <CodeDiff codeForDiff={codeForDiff} answerCode={answerCode} showAnswer={showAnswer} />
+          </CodeEditor>
+        </Col>
+      </Row>
+    );
+  }
+
+  // Controls the visibility of answer code
+  public toggleShowAnswer = (): void => {
+    this.setState({ showAnswer: !this.state.showAnswer });
+  };
+
+  // Updates code for hint
+  public submitCode = (codeForDiff: string, cb): void => {
+    this.setState({ codeForDiff }, cb);
+  };
+
+  // Checks the code written by user if it's correct
+  public checkAnswer = (code: string): void => {
+    const { answerCode } = this.props;
+    const isCorrect = this.compareAnswer(code, answerCode);
+    if (isCorrect) {
+      alert('Correct!');
+    } else {
+      alert('Try again!');
+    }
+  };
+
+  // Compares code written by user and the answer
+  private compareAnswer = (submitted: string, answer: string): boolean => {
+    return submitted === answer;
+  };
+}
