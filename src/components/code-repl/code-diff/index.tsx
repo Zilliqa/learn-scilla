dee3fc3457bf4e92f9353687185b29f0c0b74b4c
noelyoo <innovation.noel@gmail.com>
2018-11-10 11:58:15 +0900

    Refactor code diff

--- src/components/code-repl/code-diff/index.tsx
+++ src/components/code-repl/code-diff/index.tsx
@@ -3,27 +3,34 @@ import { MonacoDiffEditor } from 'react-monaco-editor';
 import './index.css';
 
 interface IProps {
+  showAnswer: boolean;
   codeForDiff: string;
   answerCode: string;
-  showAnswer: boolean;
 }
 
-const CodeDiff: React.SFC<IProps> = (props) => (
-  <div className="code-diff-container">
-    <div className={props.showAnswer ? 'hide-answer' : ''}>
-      <MonacoDiffEditor
-        width="600"
-        height="250"
-        language="javascript"
-        original={props.codeForDiff}
-        value={props.answerCode}
-        options={{
-          readOnly: true,
-          renderSideBySide: false
-        }}
-      />
+// Renders diff editor
+const CodeDiff: React.SFC<IProps> = (props) => {
+  const options = {
+    readOnly: true,
+    renderSideBySide: false
+  };
+
+  // This className decide code visibility
+  const codeVisibleStyle = props.showAnswer ? 'show-answer' : '';
+  return (
+    <div className="code-diff-container">
+      <div className={codeVisibleStyle}>
+        <MonacoDiffEditor
+          width="600"
+          height="250"
+          language="javascript"
+          original={props.codeForDiff}
+          value={props.answerCode}
+          options={options}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CodeDiff;
