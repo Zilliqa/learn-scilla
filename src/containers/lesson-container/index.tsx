a85ecd295878b12209219ae85752c981299b6afd
noelyoo <innovation.noel@gmail.com>
2018-11-11 02:14:20 +0900

    Style lesson list

--- src/containers/lesson-container/index.tsx
+++ src/containers/lesson-container/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
 import Layout from '../../components/layout';
-import { Container } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import * as H from 'history';
 import uuidv4 from 'uuid/v4';
@@ -22,14 +22,25 @@ export class LessonContainer extends React.Component<IProps, {}> {
     const { location, history, codes, t } = this.props;
     const lang = 'en';
     const lessonKeys = Object.keys(codes);
+
+    const lessonList = lessonKeys.map((lessonKey, index) => (
+      <Link
+        key={uuidv4()}
+        className="btn btn-outline-primary btn-block"
+        to={`/${lang}/lesson/${index + 1}/chapter/${1}`}
+      >
+        {`Lesson ${index + 1}`}
+      </Link>
+    ));
+
     return (
       <Layout location={location} history={history}>
         <Container>
-          {lessonKeys.map((lessonKey, index) => (
-            <div key={uuidv4()}>
-              <Link to={`/${lang}/lesson/${index + 1}/chapter/${1}`}>{lessonKey}</Link>
-            </div>
-          ))}
+          <Row className="py-5">
+            <Col sm={10} md={8} lg={5} className="mr-auto ml-auto text-center">
+              {lessonList}
+            </Col>
+          </Row>
         </Container>
       </Layout>
     );
