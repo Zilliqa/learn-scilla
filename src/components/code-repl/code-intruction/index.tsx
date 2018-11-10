d16d2b5375af58ae79aa80248a9ee9ae6caec3a3
noelyoo <innovation.noel@gmail.com>
2018-11-10 18:58:40 +0900

    Add navigation and discuss buttons

--- src/components/code-repl/code-intruction/index.tsx
+++ src/components/code-repl/code-intruction/index.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import { Button, ButtonGroup } from 'reactstrap';
+import './index.css';
 
 interface IProps {
+  t: (key: string) => string;
   instruction: string;
 }
 
-const CodeInstruction: React.SFC<IProps> = (props) => <ReactMarkdown source={props.instruction} />;
+const CodeInstruction: React.SFC<IProps> = (props) => (
+  <div>
+    <div className="code-instruction-container">
+      <ReactMarkdown source={props.instruction} />
+    </div>
+    <div className="text-center">
+      <ButtonGroup>
+        <Button outline={true} color="secondary" size="sm">
+          {props.t('editor.back')}
+        </Button>
+        <Button outline={true} color="secondary" size="sm">
+          {props.t('editor.next')}
+        </Button>
+      </ButtonGroup>{' '}
+      <Button outline={true} color="secondary" size="sm">
+        {props.t('editor.discuss')}
+      </Button>
+    </div>
+  </div>
+);
 export default CodeInstruction;
