ae6c36e732b1fe03355f9d4b0dd642948c271c82
noelyoo <innovation.noel@gmail.com>
2018-11-14 01:20:30 +0900

    Refactor reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,12 @@
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import firebase from 'firebase';
-import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
-import { reduxFirestore, firestoreReducer } from 'redux-firestore';
+import { reactReduxFirebase } from 'react-redux-firebase';
+import { reduxFirestore } from 'redux-firestore';
 import 'firebase/database';
 import 'firebase/firestore'; // make sure you add this for firestore
 
 import { firebaseConfig, reactReduxFirebaseConfig } from './config';
-
-import course from './course/reducer';
+import rootReducer from './reducer';
 
 // Initialize Firebase with config
 firebase.initializeApp(firebaseConfig);
@@ -15,15 +14,7 @@ firebase.initializeApp(firebaseConfig);
 // Initialize Firestore with timeshot settings
 firebase.firestore().settings({ timestampsInSnapshots: true });
 
-// Add firebase and firestore to reducers
-const rootReducer = combineReducers({
-  firebase: firebaseReducer,
-  firestore: firestoreReducer,
-  course
-});
-
 let middlewares: any[] = [];
-
 // Use redux logger only when env is development
 if (process.env.NODE_ENV === 'development') {
   // tslint:disable-next-line
@@ -35,7 +26,6 @@ export const store = createStore(
   rootReducer,
   compose(
     reduxFirestore(firebase),
-    // Firebase instance as first argument
     reactReduxFirebase(firebase, reactReduxFirebaseConfig),
     applyMiddleware(...middlewares)
   )
