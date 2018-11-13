0973d1dc5e0a73367dacbb60cd381236d4c18f2f
noelyoo <innovation.noel@gmail.com>
2018-11-13 23:53:27 +0900

    Add comment for Firebase apikey

--- src/redux/config.ts
+++ src/redux/config.ts
@@ -1,4 +1,8 @@
-// Firebase config
+/*
+  The apiKey essentially identifies your Firebase project. It is not a security risk for someone to know it.
+  https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public
+*/
+
 export const firebaseConfig = {
   apiKey: 'AIzaSyBjcNRjbN9ZsWHxY8u3uSXeMMoXkoikPug',
   authDomain: 'learn-scilla.firebaseapp.com',
