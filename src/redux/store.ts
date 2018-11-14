f33da70d3badb57d7f1f09c7c1c7e4ed984ce9e9
noelyoo <innovation.noel@gmail.com>
2018-11-14 11:21:37 +0900

    Fix Firebase import

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import firebase from 'firebase';
-import { reactReduxFirebase } from 'react-redux-firebase';
-import { reduxFirestore } from 'redux-firestore';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/firestore'; // make sure you add this for firestore
-
+import { reactReduxFirebase } from 'react-redux-firebase';
+import { reduxFirestore } from 'redux-firestore';
 import { firebaseConfig, reactReduxFirebaseConfig } from './config';
 import rootReducer from './reducer';
+import { logger } from 'redux-logger';
 
 // Initialize Firebase with config
 firebase.initializeApp(firebaseConfig);
@@ -14,16 +15,14 @@ firebase.initializeApp(firebaseConfig);
 // Initialize Firestore with timeshot settings
 firebase.firestore().settings({ timestampsInSnapshots: true });
 
-let middlewares: any[] = [];
 // Use redux logger only when env is development
-if (process.env.NODE_ENV === 'development') {
-  // tslint:disable-next-line
-  const { logger } = require('redux-logger');
-  middlewares = [logger];
-}
+const isDev = process.env.NODE_ENV === 'development';
+const middlewares = isDev ? [logger] : [];
 
 export const store = createStore(
+  // reducer
   rootReducer,
+  // enhancer
   compose(
     reduxFirestore(firebase),
     reactReduxFirebase(firebase, reactReduxFirebaseConfig),
