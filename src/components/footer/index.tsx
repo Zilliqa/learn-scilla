0cb438b3982d2f60bef0cbcf787f3b93e87a0ebe
noelyoo <innovation.noel@gmail.com>
2018-11-20 19:34:43 +0900

    Add links in footer: github, gitter, telegram

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import styles from './footer.module.css';
-
+import { Nav, NavItem, NavLink } from 'reactstrap';
+import { FaGithub, FaGitter, FaTelegramPlane } from 'react-icons/fa';
 const copyright: string = 'Copyright © LearnScilla 2018';
 
 const Footer: React.SFC<{}> = () => (
   <footer className={styles.footer}>
-    <span className={styles.copyright}>{copyright}</span>
+    <div className="text-center">
+      <span className={styles.copyright}>{copyright}</span>
+      <Nav style={{ justifyContent: 'center' }}>
+        <NavItem>
+          <NavLink href="https://github.com/Zilliqa" target="_blank">
+            <FaGithub />
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="https://gitter.im/Zilliqa" target="_blank">
+            <FaGitter />
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="https://t.me/zilliqachat" target="_blank">
+            <FaTelegramPlane />
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </div>
   </footer>
 );
 
