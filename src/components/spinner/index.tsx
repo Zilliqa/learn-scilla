5d32c09669228d6a171bc439dfaed36696eee195
noelyoo <innovation.noel@gmail.com>
2018-11-21 16:45:56 +0900

    Refactor SFCs

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './spinner.module.css';
 
 // Renders spinner to visualize loading status
-const Spinner: React.SFC<{}> = () => (
+const Spinner: React.SFC = () => (
   <div className="py-5 text-center">
     <div className={styles.spinner} />
   </div>
