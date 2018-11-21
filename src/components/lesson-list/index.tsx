589759496dcd7be497821ff9996016f65cab0a1c
noelyoo <innovation.noel@gmail.com>
2018-11-21 16:50:12 +0900

    Refactor lesson list

--- src/components/lesson-list/index.tsx
+++ src/components/lesson-list/index.tsx
@@ -12,9 +12,12 @@ const LessonList: React.SFC<any> = (props) => {
   return list.map((item, index) => {
     const lessonNum: number = index + 1;
     const lessonKey: string = `lesson${lessonNum}`;
+
     const progressProfile = profile.progress || {};
     const lessonProgressNum: number = progressProfile[lessonKey] || 0;
+
     const isLoaded: boolean = profile.isLoaded;
+
     const chapters: string[] = item.chapters || [];
     const totalNum: number = chapters.length;
 
@@ -22,20 +25,21 @@ const LessonList: React.SFC<any> = (props) => {
     const progressPercent = Math.floor((lessonProgressNum / totalNum) * 100);
     const chapterToStart = totalNum <= lessonProgressNum ? totalNum : lessonProgressNum + 1;
 
+    const startingLessonPath = `/lesson/${lessonNum}/chapter/${chapterToStart}`;
+
+    const trailColor = isLoaded ? '#D9D9D9' : 'transparent';
+    const strokeColor = '#007bff';
     return (
       <div key={uuidv4()}>
-        <Link
-          className="btn btn-outline-primary btn-block text-left"
-          to={`/lesson/${lessonNum}/chapter/${chapterToStart}`}
-        >
+        <Link className="btn btn-outline-primary btn-block text-left" to={startingLessonPath}>
           {`${t('lesson.lesson')} ${lessonNum}`}: {`${item.title} ${progressText}`}
         </Link>
         <Line
           style={{ marginTop: -15 }}
           percent={`${progressPercent}`}
           strokeWidth={1}
-          strokeColor={'#007bff'}
-          trailColor={isLoaded ? '#D9D9D9' : 'transparent'}
+          strokeColor={strokeColor}
+          trailColor={trailColor}
         />
       </div>
     );
