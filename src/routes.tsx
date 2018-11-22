5b54e5d9e6ba7f895a80b94c655ca8e8f0235f00
noelyoo <innovation.noel@gmail.com>
2018-11-22 13:27:53 +0900

    Use React.lazy for code-splitting

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,46 +1,29 @@
-import React from 'react';
-import { Router, Route, Switch, Redirect } from 'react-router-dom';
+// @ts-ignore
+import React, { Suspense, lazy } from 'react';
 
-import createHistory from 'history/createBrowserHistory';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+const LessonCompleteContainer = lazy(() => import('./containers/lesson-complete-container'));
+const ChapterContainer = lazy(() => import('./containers/chapter-container'));
 
 import LessonListContainer from './containers/lesson-list-container';
-import LessonCompleteContainer from './containers/lesson-complete-container';
-import ChapterContainer from './containers/chapter-container';
-
-export const appHistory = createHistory();
+import Spinner from './components/spinner';
 
 export const paths = {
-  home: '/home',
   lessonList: '/lesson-list',
   lessonComplete: '/lesson-complete/:lesson',
-  chapter: '/lesson/:lesson/chapter/:chapter',
-  signin: '/signin'
+  chapter: '/lesson/:lesson/chapter/:chapter'
 };
 
-export const routeList = [
-  {
-    path: paths.lessonList,
-    component: LessonListContainer
-  },
-  {
-    path: paths.lessonComplete,
-    component: LessonCompleteContainer
-  },
-  {
-    path: paths.chapter,
-    component: ChapterContainer
-  }
-];
-
 export const RouterNode: React.SFC = () => (
-  <Router history={appHistory}>
-    <div>
+  <Router>
+    <Suspense fallback={<Spinner />}>
       <Switch>
-        {routeList.map((prop, key) => (
-          <Route exact={true} path={prop.path} key={key} component={prop.component} />
-        ))}
+        <Route exact={true} path={paths.lessonList} component={LessonListContainer} />
+        <Route exact={true} path={paths.chapter} component={ChapterContainer} />
+        <Route exact={true} path={paths.lessonComplete} component={LessonCompleteContainer} />
         <Redirect from="/" to={paths.lessonList} />
       </Switch>
-    </div>
+    </Suspense>
   </Router>
 );
