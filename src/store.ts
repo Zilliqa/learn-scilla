5d2936cacd79d460646cfb59fe6912e46337e6e8
noelyoo <innovation.noel@gmail.com>
2018-11-23 21:33:09 +0900

    Restructure directory

--- src/store.ts
+++ src/store.ts
@@ -5,9 +5,22 @@ import 'firebase/database';
 import 'firebase/firestore'; // make sure you add this for firestore
 import { reactReduxFirebase } from 'react-redux-firebase';
 import { reduxFirestore } from 'redux-firestore';
-import { firebaseConfig, reactReduxFirebaseConfig } from './config';
-import rootReducer from './reducer';
+
+import rootReducer from './reducers';
 import { logger } from 'redux-logger';
+
+/*
+  The apiKey essentially identifies your Firebase project. It is not a security risk for someone to know it.
+  https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public
+*/
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBjcNRjbN9ZsWHxY8u3uSXeMMoXkoikPug',
+  authDomain: 'learn-scilla.firebaseapp.com',
+  databaseURL: 'https://learn-scilla.firebaseio.com',
+  projectId: 'learn-scilla',
+  storageBucket: 'learn-scilla.appspot.com',
+  messagingSenderId: '825204243213'
+};
 
 // Initialize Firebase with config
 firebase.initializeApp(firebaseConfig);
@@ -18,6 +31,12 @@ firebase.firestore().settings({ timestampsInSnapshots: true });
 // Use redux logger only when env is development
 const isDev = process.env.NODE_ENV === 'development';
 const middlewares = isDev ? [logger] : [];
+
+// react-redux-firebase config
+const reactReduxFirebaseConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+};
 
 export const store = createStore(
   // reducer
--- src/utils/helper.spec.ts
+++
@@ -1,5 +0,0 @@
-import { changeQueryStringToJSON } from './helpers';
-
-test('test changeQueryStringToJSON', () => {
-  expect(changeQueryStringToJSON('a=0&b=text')).toEqual({ a: '0', b: 'text' });
-});
