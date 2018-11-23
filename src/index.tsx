df4ab51c9c324c989cfa9a37aacfca77d7cfb5c4
noelyoo <innovation.noel@gmail.com>
2018-11-23 13:12:26 +0900

    Move i18n folder

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
 import { I18nextProvider } from 'react-i18next';
-import i18n from './utils/i18n';
+import i18n from './i18n';
 import { RouterNode } from './routes';
 import * as serviceWorker from './serviceWorker';
 
