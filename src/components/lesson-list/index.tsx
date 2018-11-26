f4adb9b9cebae4f87e57edc815face3f7b959fcb
noelyoo <innovation.noel@gmail.com>
2018-11-26 13:34:04 +0900

    Show spinner until progress is loaded

--- src/components/lesson-list/index.tsx
+++ src/components/lesson-list/index.tsx
@@ -3,20 +3,26 @@ import { Link } from 'react-router-dom';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { withFirebase } from 'react-redux-firebase';
+import Spinner from '../spinner';
 
 const LessonList: React.SFC<any> = (props) => {
   const { t, profile, lessonList } = props;
   const list = lessonList || [];
+
+  const isLoaded: boolean = profile.isLoaded;
+  const isAuth: boolean = !profile.isEmpty;
+  const showProgressText = isLoaded && isAuth;
+
+  if (!isLoaded) {
+    return <Spinner />;
+  }
+
   return list.map((item, index) => {
     const lessonNum: number = index + 1;
     const lessonKey: string = `lesson${lessonNum}`;
 
     const progressProfile = profile.progress || {};
     const lessonProgressNum: number = progressProfile[lessonKey] || 0;
-
-    const isLoaded: boolean = profile.isLoaded;
-    const isAuth: boolean = !profile.isEmpty;
-    const showProgressText = isLoaded && isAuth;
 
     const chapters: string[] = item.chapters || [];
     const totalNum: number = chapters.length;
