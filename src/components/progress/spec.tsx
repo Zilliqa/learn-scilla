9816373f3380365ab26ec2877b9f636055df68c7
noelyoo <innovation.noel@gmail.com>
2018-12-05 16:10:42 +0900

    Improve the style of progressbar

--- src/components/progress/spec.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Progress from '.';
-import * as renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  const tree = renderer.create(<Progress value={5} max={10} color={'secondary'} />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
