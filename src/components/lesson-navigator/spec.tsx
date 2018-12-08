5afb3ddcfbcac8750487bb7db03465a5978b7185
noelyoo <innovation.noel@gmail.com>
2018-12-08 15:26:37 +0900

    Add basic unit tests

--- src/components/lesson-navigator/spec.tsx
+++ src/components/lesson-navigator/spec.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import LessonNavigator from '.';
+import ReactDOM from 'react-dom';
 import * as renderer from 'react-test-renderer';
+import LessonNavigator from '.';
 
 const goNext = () => console.log('goNext');
 const goBack = () => console.log('goBack');
 const t = (s: string) => s;
 
-it('renders correctly while loading', () => {
-  const tree = renderer
-    .create(<LessonNavigator lessonNumber={5} total={10} goNext={goNext} goBack={goBack} t={t} />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Lesson Navigator tests', () => {
+  const baseComponent = () => (
+    <LessonNavigator lessonNumber={5} total={10} goNext={goNext} goBack={goBack} t={t} />
+  );
+
+  describe('basic tests', () => {
+    it('matches the snapshot', () => {
+      const tree = renderer.create(baseComponent()).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(baseComponent(), div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+  });
 });
