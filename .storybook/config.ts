d033f262f3b5d82fd7a991091743ad5d18d01551
noelyoo <innovation.noel@gmail.com>
2018-12-09 14:31:27 +0900

    Add button stories

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,5 +1,8 @@
 import { configure } from '@storybook/react';
 // automatically import all files ending in *.stories.tsx
+import 'bootstrap/dist/css/bootstrap.css';
+import '../src/index.css';
+
 const req = require.context('../src', true, /.stories.tsx$/);
 
 function loadStories() {
