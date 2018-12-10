568099d8e6a11e6d396a1868277febaf3fa6fb1a
noelyoo <innovation.noel@gmail.com>
2018-12-10 14:08:06 +0900

    Improve chapter complete card stories

--- src/components/chapter-complete-card/component.stories.tsx
+++ src/components/chapter-complete-card/component.stories.tsx
@@ -8,4 +8,5 @@ const t = (s) => s;
 
 storiesOf('Chapter Complete Card', module)
   .addDecorator((story) => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>)
-  .add('default', () => <ChapterCompleteCard t={t} total={2} chapter={1} />);
+  .add('default', () => <ChapterCompleteCard t={t} total={2} chapter={1} />)
+  .add('with the last chapter', () => <ChapterCompleteCard t={t} total={1} chapter={1} />);
