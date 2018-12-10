a2be54c7d475d48f3bb04145c6ad7aee17c55381
noelyoo <innovation.noel@gmail.com>
2018-12-10 23:25:49 +0900

    Add color, icon stories

---
+++ src/colors.ts
@@ -0,0 +1,12 @@
+const colors = {
+  white: '#ffffff',
+  black: '#131520',
+  blue500: '#3740ff',
+  blue600: '#3038e0',
+  red500: '#e2051a',
+  red600: '#be032a',
+  gray500: '#7c7d8b',
+  gray600: '#585967'
+};
+
+export default colors;
