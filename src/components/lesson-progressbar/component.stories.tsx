19e035eafd1030d90284d4e0c0d82aeed2b43d8b
noelyoo <innovation.noel@gmail.com>
2018-12-15 15:53:09 +0900

    Allow to navigate lessons with progressbar

--- src/components/lesson-progressbar/component.stories.tsx
+++ src/components/lesson-progressbar/component.stories.tsx
@@ -3,7 +3,17 @@ import { storiesOf } from '@storybook/react';
 
 import LessonProgressbar from '.';
 
+const navigate = (chapterNum, lessonNum) => {
+  console.log(`navigate to: /chapter/${chapterNum}/lesson/${lessonNum}`);
+};
+
 storiesOf('Lesson Progressbar', module)
-  .add('default', () => <LessonProgressbar current={2} total={5} />)
-  .add('min', () => <LessonProgressbar current={0} total={5} />)
-  .add('max', () => <LessonProgressbar current={5} total={5} />);
+  .add('default', () => (
+    <LessonProgressbar navigate={navigate} chapterNumber={1} lessonNumber={3} total={5} />
+  ))
+  .add('min', () => (
+    <LessonProgressbar navigate={navigate} chapterNumber={1} lessonNumber={1} total={5} />
+  ))
+  .add('max', () => (
+    <LessonProgressbar navigate={navigate} chapterNumber={1} lessonNumber={5} total={5} />
+  ));
--- src/components/lesson-progressbar/index.tsx
+++ src/components/lesson-progressbar/index.tsx
@@ -1,32 +1,103 @@
 import React from 'react';
-import { Progress } from 'reactstrap';
+
 import './style.css';
+import classNames from 'classnames';
 
 interface IProps {
-  current: number;
+  lessonNumber: number;
+  chapterNumber: number;
   total: number;
+  navigate: (chapterNum: number, lessonNum: number) => void;
 }
 
 // Renders step progressbar dynamically
-const LessonProgressbar: React.SFC<IProps> = ({ current, total }) => {
+const LessonProgressbar: React.SFC<IProps> = ({ navigate, chapterNumber, lessonNumber, total }) => {
   // initialized array with the given total number
   const list = Array.from({ length: total });
   const percent = (1 / total) * 100;
 
   return (
     <Progress multi={true} className="lesson-progress-bar-container">
-      {list.map((item, index) => (
-        <Progress
-          key={index}
-          barClassName="lesson-progress-bar"
-          bar={true}
-          color={current < index ? 'secondary' : 'primary'}
-          value={percent}
-          max={total}
-        />
-      ))}
+      {list.map((item, index) => {
+        const isLocked = lessonNumber <= index;
+        const color = isLocked ? 'secondary' : 'primary';
+        return (
+          <Progress
+            key={index}
+            onClick={() => navigate(chapterNumber, index + 1)}
+            barClassName={`lesson-progress-bar cursor-pointer`}
+            bar={true}
+            color={color}
+            value={percent}
+            max={total}
+          />
+        );
+      })}
     </Progress>
   );
 };
 
 export default LessonProgressbar;
+
+interface IProgressProps {
+  onClick?: () => void;
+  children?: any;
+  bar?: boolean;
+  multi?: boolean;
+  value?: number;
+  max?: number;
+  animated?: boolean;
+  striped?: boolean;
+  color?: string;
+  className?: string;
+  barClassName?: string;
+}
+
+const Progress = (props: IProgressProps) => {
+  const {
+    children,
+    className,
+    barClassName,
+    value = 0,
+    max = 100,
+    animated,
+    striped,
+    color,
+    bar,
+    multi,
+    onClick,
+    ...attributes
+  } = props;
+
+  const percent = (value / max) * 100;
+  const progressClasses = classNames(className, 'progress');
+
+  const progressBarClasses = classNames(
+    'progress-bar',
+    bar ? className || barClassName : barClassName,
+    animated ? 'progress-bar-animated' : null,
+    color ? `bg-${color}` : null,
+    striped || animated ? 'progress-bar-striped' : null
+  );
+
+  const ProgressBar = multi ? (
+    children
+  ) : (
+    <div
+      className={progressBarClasses}
+      style={{ width: `${percent}%` }}
+      role="progressbar"
+      onClick={onClick}
+      aria-valuenow={value}
+      aria-valuemin={0}
+      aria-valuemax={max}
+      children={children}
+    />
+  );
+
+  if (bar) {
+    return ProgressBar;
+  }
+
+  return <div {...attributes} className={progressClasses} children={ProgressBar} />;
+};
