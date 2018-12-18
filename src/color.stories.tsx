dc334e47655f0e5370c75201da6e3f16a1cb977d
noelyoo <innovation.noel@gmail.com>
2018-12-18 13:44:18 +0900

    Add more colors

--- src/color.stories.tsx
+++ src/color.stories.tsx
@@ -4,7 +4,7 @@ import colors from './colors';
 
 const keyList = Object.keys(colors);
 const getStyle = (key) => ({
-  color: key === 'white' ? 'black' : 'white',
+  color: key === 'white' || key === 'grey200' ? 'black' : 'white',
   backgroundColor: colors[key]
 });
 
@@ -12,7 +12,7 @@ storiesOf('Color', module).add('All Colors', () => (
   <div>
     {keyList.map((key) => {
       return (
-        <p key={key} className="text-center justify-content-center py-2" style={getStyle(key)}>
+        <p key={key} className="text-center justify-content-center py-1" style={getStyle(key)}>
           {key}
           <br />
           <small>{colors[key]}</small>
--- src/colors.ts
+++ src/colors.ts
@@ -1,12 +1,17 @@
 const colors = {
   white: '#ffffff',
+  grey200: '#e9e9f4',
+  gray500: '#7c7d8b',
+  gray600: '#585967',
   black: '#131520',
   blue500: '#3740ff',
   blue600: '#3038e0',
+  green500: '#148e50',
+  green600: '#0d6137',
+  yellow500: '#e9ab18',
+  yellow600: '#d19200',
   red500: '#e2051a',
-  red600: '#be032a',
-  gray500: '#7c7d8b',
-  gray600: '#585967'
+  red600: '#be032a'
 };
 
 export default colors;
