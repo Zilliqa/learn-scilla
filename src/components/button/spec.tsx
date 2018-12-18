61d234ea98c7714d30b99337e85803ae9a0c161a
noelyoo <innovation.noel@gmail.com>
2018-12-18 15:38:46 +0900

    Add more button tests

--- src/components/button/spec.tsx
+++ src/components/button/spec.tsx
@@ -5,27 +5,45 @@ import { FaCheck } from 'react-icons/fa';
 import Button from '.';
 
 describe('Footer tests', () => {
-  const baseComponent = (props?: object) => (
+  const baseComponent = (props) => (
     <Button
-      type="secondary"
+      type={props.type}
       text={'ButtonText'}
       disabled={false}
       onClick={() => console.log('click')}
       ariaLabel={'Test Button'}
       icon={{ image: <FaCheck />, position: 'before' }}
-      size="sm"
+      size={'md'}
     />
   );
 
   describe('basic tests', () => {
     it('matches the snapshot', () => {
-      const tree = renderer.create(baseComponent()).toJSON();
+      const tree = renderer.create(baseComponent({ type: 'secondary' })).toJSON();
       expect(tree).toMatchSnapshot();
     });
 
-    it('renders without crashing', () => {
+    it('renders primary button without crashing', () => {
       const div = document.createElement('div');
-      ReactDOM.render(baseComponent(), div);
+      ReactDOM.render(baseComponent({ type: 'primary' }), div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders secondary button without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(baseComponent({ type: 'secondary' }), div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders warning button crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(baseComponent({ type: 'warning' }), div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders danger button without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(baseComponent({ type: 'danger' }), div);
       ReactDOM.unmountComponentAtNode(div);
     });
   });
