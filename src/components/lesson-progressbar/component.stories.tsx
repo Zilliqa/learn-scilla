a5508ce82d5736387c192e9e8d10232d02da8929
noelyoo <innovation.noel@gmail.com>
2018-12-21 18:48:52 +0900

    Add Chapter title

--- src/components/lesson-progressbar/component.stories.tsx
+++ src/components/lesson-progressbar/component.stories.tsx
@@ -2,18 +2,18 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import LessonProgressbar from '.';
-
+const t = (s) => s;
 const navigate = (chapterNum, lessonNum) => {
   console.log(`navigate to: /chapter/${chapterNum}/lesson/${lessonNum}`);
 };
 
 storiesOf('Lesson Progressbar', module)
   .add('default', () => (
-    <LessonProgressbar navigate={navigate} chapterNumber={1} lessonNumber={3} total={5} />
+    <LessonProgressbar t={t} navigate={navigate} chapterNumber={1} lessonNumber={3} total={5} />
   ))
   .add('min', () => (
-    <LessonProgressbar navigate={navigate} chapterNumber={1} lessonNumber={1} total={5} />
+    <LessonProgressbar t={t} navigate={navigate} chapterNumber={1} lessonNumber={1} total={5} />
   ))
   .add('max', () => (
-    <LessonProgressbar navigate={navigate} chapterNumber={1} lessonNumber={5} total={5} />
+    <LessonProgressbar t={t} navigate={navigate} chapterNumber={1} lessonNumber={5} total={5} />
   ));
