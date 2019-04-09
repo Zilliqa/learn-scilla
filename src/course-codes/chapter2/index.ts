d1332d18a62f469563c8ce28311c8afd81c63c55
noelyoo <innovation.noel@gmail.com>
2019-04-09 15:28:31 +0900

    feat(course): update ch2 instructions and codes

--- src/course-codes/chapter2/index.ts
+++ src/course-codes/chapter2/index.ts
@@ -1,198 +1,385 @@
 import { ILessonCode, ChapterCodeType } from '../../typings';
 
 const l1: ILessonCode = {
-  initialCode: `contract Zealgame
+  initialCode: `scilla_version 0
 
-(owner: ByStr20)
-(*Start typing from the line below.*)
+contract SocialMediaPayment (owner: ByStr20)
+(*Start typing from the line below*)
+`,
+  answerCode: `scilla_version 0
 
-Transition changeName(newname: String)
+contract SocialMediaPayment (owner: ByStr20)
+(*Start typing from the line below*)
 
-end`,
-  answerCode: `contract Zealgame
+transition deposit()
 
-(owner: ByStr20)
-(*Start typing from the line below.*)
-field map_Name : Map Address String = Emp Address String
-
-Transition changeName(newname: String)
-
-end`
+end 
+`
 };
 
 const l2: ILessonCode = {
-  initialCode: `contract Zealgame
-(owner: ByStr20)
-field map_Name : Map Address String = Emp Address String (*Address map to a playerName*)
+  initialCode: `scilla_version 0
 
-Transition changeName(newname: String)
+(*Start typing from the line below*)
 
-(*Start typing from the line below.*)
 
-end`,
-  answerCode: `contract Zealgame
-(owner: ByStr20)
-field map_Name : Map Address String = Emp Address String (*Address map to a playerName*)
+contract SocialMediaPayment(owner: ByStr20)
 
-Transition changeName(newname: String)
+transition deposit()
 
-(*Start typing from the line below.*)
-r <- map_Name
+end
+`,
+  answerCode: `scilla_version 0
+
+(*Start typing from the line below*)
+import BoolUtils
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
 
 end`
 };
 
 const l3: ILessonCode = {
-  initialCode: `contract Zealgame
-(owner: ByStr20)
-(* Start typing from the line below. *)
+  initialCode: `scilla_version 0
+  import BoolUtils
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+(*Start typing from the line below. The answer will be a single line within the transition so you won’t need to use semicolon at the end*)
+
+end
 `,
-  answerCode: `contract Zealgame
-(owner: ByStr20)
-(* Start typing from the line below. *)
-field player_name : String = "Alice"`
+  answerCode: `scilla_version 0
+  import BoolUtils
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+(*Start typing from the line below. The answer will be a single line within the transition so you won’t need to use semicolon at the end*)
+
+sender_is_owner = builtin eq _sender owner
+
+
+end`
 };
 
 const l4: ILessonCode = {
-  initialCode: `contract Zealgame
-(owner: ByStr20)
-field player_name : String = "Alice"
+  initialCode: `scilla_version 0
+  import BoolUtils
 
-(* Start typing from the line below *)
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner
+(*Start typing from the line below. *)
+
+end
 `,
-  answerCode: `contract Zealgame
-(owner: ByStr20)
-field player_name : String = "Alice"
+  answerCode: `scilla_version 0
+  import BoolUtils
 
-(* Start typing from the line below *)
-Transition changeName()
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+(*Start typing from the line below. *)
+match sender_is_owner with
+     | False =>
+
+     | True =>
+
+     end
 end`
 };
 
 const l5: ILessonCode = {
-  initialCode: `contract Zealgame
-(owner: ByStr20)
-field player_name : String = "Alice"
+  initialCode: `scilla_version 0
+  import BoolUtils
+  (*Start typing from the line below. First define the library*)
+  
+  (*Now define the variable zero for Uint128 0*)
 
-Transition changeName()
-(* Start typing from the line below *)
-end`,
-  answerCode: `contract Zealgame
-(owner: ByStr20)
-field player_name : String = "Alice"
 
-Transition changeName()
-(* Start typing from the line below *)
-newname = "Bob"
+  (*Now, in the line below, define the variable not_owner_code of type Uint32 and value 1*)
+
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+match sender_is_owner with
+     | False =>
+
+     | True =>
+
+     end
+end
+`,
+  answerCode: `
+  scilla_version 0
+  import BoolUtils
+  (*Start typing from the line below. First define the library*)
+  library SocialMediaPayment
+  
+  (*Now define the variable zero for Uint128 0*)
+  let zero = Uint128 0
+
+
+  (*Now, in the line below, define the variable not_owner_code of type Uint32 and value 1*)
+  let not_owner_code = Uint32 1
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+match sender_is_owner with
+     | False =>
+
+     | True =>
+
+     end
 end`
 };
 
 const l6: ILessonCode = {
-  initialCode: `contract Zealgame
-(owner: ByStr20)
-field player_name : String = "Alice"
+  initialCode: `
+  scilla_version 0
+  import BoolUtils
+  
+  library SocialMediaPayment
+  (*Start typing from the line below. Copy paste the code given in the first task below to define the variable one_msg*)
+  
+  
+  let zero = Uint128 0
+  let not_owner_code = Uint32 1
 
-Transition changeName()
-(* Now that we’ll be having another line, don’t forget to include a semicolon at the end of this line *)
-Newname = "Bob"
+contract SocialMediaPayment(owner: ByStr20)
 
-(* Start typing from the line below.  *)
-
-end`,
-  answerCode: `contract Zealgame
-(owner: ByStr20)
-field player_name : String = "Alice"
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+match sender_is_owner with
+     | False =>
+     (*Start typing from the line below. Define the variable msg below with the values given in the second point of the task*)
 
 
-Transition changeName()
-(* Now that we’ll be having another line, don’t forget to include a semicolon at the end of this line *)
-newname = "Bob";
+    (*Start typing from the line below. Copy the two lines given in the third point of the task below. *)
 
-(* Start typing from the line below.  *)
-Player_name := newname
 
-end`
+     | True =>
+
+     end
+end
+`,
+  answerCode: `
+  scilla_version 0
+  import BoolUtils
+  
+  library SocialMediaPayment
+  (*Start typing from the line below. Copy paste the code given in the first task below to define the variable one_msg*)
+  let one_msg = 
+    fun (msg : Message) => 
+    let nil_msg = Nil {Message} in
+    Cons {Message} msg nil_msg
+  
+  let zero = Uint128 0
+  let not_owner_code = Uint32 1
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+match sender_is_owner with
+     | False =>
+     (*Start typing from the line below. Define the variable msg below with the values given in the second point of the task*)
+
+          msg = {_tag: "";
+                _recipient: _sender;
+                _amount: zero;
+                code: not_owner_code};
+    (*Start typing from the line below. Copy the two lines given in the third point of the task below. *)
+          msgs = one_msg msg;
+          send msgs
+
+     | True =>
+
+     end
+end
+  `
 };
 
 const l7: ILessonCode = {
-  initialCode: `contract Zealgame
-(owner: ByStr20)
-field player_name : String = "Alice"
+  initialCode: `
+    scilla_version 0
+  import BoolUtils
+  
+  library SocialMediaPayment
+  
+  let one_msg = 
+    fun (msg : Message) => 
+    let nil_msg = Nil {Message} in
+    Cons {Message} msg nil_msg
+  
+  let zero = Uint128 0
+  let not_owner_code = Uint32 1
+(*Start typing from the line below. declare a Uint32 type of variable with variable name “accepted_code” and value “0” *)
 
-(* Start typing in the parentheses below *)
-Transition changeName()
-player_name := newname
+
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+match sender_is_owner with
+     | False =>
+     
+          msg = {_tag: "";
+                _recipient: _sender;
+                _amount: zero;
+                code: not_owner_code};
+          msgs = one_msg msg;
+          send msgs
+
+     | True =>
+        (*Start typing from the line below for task 2, 3 and 4*)
+        
+        
+     end
 end
 `,
-  answerCode: `contract Zealgame
-(owner: ByStr20)
-field player_name : String = "Alice"
+  answerCode: `
+ scilla_version 0
+  import BoolUtils
+  
+  library SocialMediaPayment
+  
+  let one_msg = 
+    fun (msg : Message) => 
+    let nil_msg = Nil {Message} in
+    Cons {Message} msg nil_msg
+  
+  let zero = Uint128 0
+  let not_owner_code = Uint32 1
+    (*Start typing from the line below. declare a Uint32 type of variable with variable name “accepted_code” and value “0” *)
+  let accepted_code = Uint32 0
 
-(* Start typing in the parentheses below *)
-Transition changeName(newname: String)
-player_name := newname
-end`
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+match sender_is_owner with
+     | False =>
+     
+          msg = {_tag: "";
+                _recipient: _sender;
+                _amount: zero;
+                code: not_owner_code};
+          msgs = one_msg msg;
+          send msgs
+
+     | True =>
+        (*Start typing from the line below for task 2, 3 and 4*)
+        accept;
+        msg = {_tag: "";
+                _recipient: _sender;
+                _amount: zero;
+                code: accepted_code};
+        msgs = one_msg msg;
+        send msgs
+        
+     end
+end
+  `
 };
 
 const l8: ILessonCode = {
-  initialCode: `contract Zealgame
-(owner: ByStr20)
-field player_name : String = "Alice"
-(* Declare the two new immutable variables below. You don’t need to use any semicolons to separate the lines outside the transitions *)
+  initialCode: `
+    scilla_version 0
+  import BoolUtils
+  
+  library SocialMediaPayment
+  
+  let one_msg = 
+    fun (msg : Message) => 
+    let nil_msg = Nil {Message} in
+    Cons {Message} msg nil_msg
+  
+  let zero = Uint128 0
+  let not_owner_code = Uint32 1
+(*Start typing from the line below. declare a Uint32 type of variable with variable name “accepted_code” and value “0” *)
 
-Transition changeName(newname: String)
 
-player_name := newname
-(* Assign the value of the implicit variables to the new immutable variables in the lines below. You’ll need to separate the lines in the transition by the use of semicolons *)
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+match sender_is_owner with
+     | False =>
+     
+          msg = {_tag: "";
+                _recipient: _sender;
+                _amount: zero;
+                code: not_owner_code};
+          msgs = one_msg msg;
+          send msgs
+
+     | True =>
+        (*Start typing from the line below for task 2, 3 and 4*)
+        
+        
+     end
 end
 `,
-  answerCode: `contract Zealgame
-(owner: ByStr20)
-field player_name : String = "Alice"
-(* Declare the two new immutable variables below. You don’t need to use any semicolons to separate the lines outside the transitions *)
-field player_address : ByStr20 = 0x1234567890123456789012345678901234567890
-field player_zeal: Uint128 = Uint128 0
+  answerCode: `
+ scilla_version 0
+  import BoolUtils
+  
+  library SocialMediaPayment
+  
+  let one_msg = 
+    fun (msg : Message) => 
+    let nil_msg = Nil {Message} in
+    Cons {Message} msg nil_msg
+  
+  let zero = Uint128 0
+  let not_owner_code = Uint32 1
+    (*Start typing from the line below. declare a Uint32 type of variable with variable name “accepted_code” and value “0” *)
+  let accepted_code = Uint32 0
 
-Transition changeName(newname: String)
 
-player_name := newname;
-(* Assign the value of the implicit variables to the new immutable variables in the lines below. You’ll need to separate the lines in the transition by the use of semicolons *)
-player_address := _sender;
-player_zeal := _amount
+contract SocialMediaPayment(owner: ByStr20)
 
-end`
-};
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+match sender_is_owner with
+     | False =>
+     
+          msg = {_tag: "";
+                _recipient: _sender;
+                _amount: zero;
+                code: not_owner_code};
+          msgs = one_msg msg;
+          send msgs
 
-const l9: ILessonCode = {
-  initialCode: `contract Zealgame
-(owner: ByStr20)
-field player_name : String = "Alice"
-field player_address : ByStr20 = 0x1234567890123456789012345678901234567890
-field player_zeal: Uint128 = Uint128 0
-
-Transition changeName(newname: String)
-
-player_name := newname;
-player_address := _sender;
-player_zeal := _amount
-
+     | True =>
+        (*Start typing from the line below for task 2, 3 and 4*)
+        accept;
+        msg = {_tag: "";
+                _recipient: _sender;
+                _amount: zero;
+                code: accepted_code};
+        msgs = one_msg msg;
+        send msgs
+        
+     end
 end
-`,
-  answerCode: `contract Zealgame
-(owner: ByStr20)
-field player_name : String = "Alice"
-field player_address : ByStr20 = 0x1234567890123456789012345678901234567890
-field player_zeal: Uint128 = Uint128 0
-
-Transition changeName(newname: String)
-
-player_name := newname;
-player_address := _sender;
-player_zeal := _amount
-
-end`
+  `
 };
 
-const chapter2: ChapterCodeType = [l1, l2, l3, l4, l5, l6, l7, l8, l9];
+const chapter2: ChapterCodeType = [l1, l2, l3, l4, l5, l6, l7, l8];
 
 export default chapter2;
