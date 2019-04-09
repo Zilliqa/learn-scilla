cfd825c414f5931306898e0647a9f86896f4b0c8
noelyoo <innovation.noel@gmail.com>
2019-04-09 19:17:14 +0900

    fix(course): add ch3 l10

--- src/locales/instructions/en/chapter3/index.ts
+++ src/locales/instructions/en/chapter3/index.ts
@@ -289,7 +289,7 @@ https://scilla.readthedocs.io/en/latest/index.html
 
 const chapter3: IChapterInstruction = {
   title: 'Intermediate - B',
-  lessons: [l1, l2, l3, l4, l5, l6, l7, l8, l9]
+  lessons: [l1, l2, l3, l4, l5, l6, l7, l8, l9, l10]
 };
 
 export default chapter3;
